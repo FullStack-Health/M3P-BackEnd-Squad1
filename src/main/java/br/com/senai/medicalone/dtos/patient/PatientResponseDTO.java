e4346e8436b7d5d547f21b21bea1d38a996ab55b
Felippe Kulkamp Sant ana <123035151+Felippeks@users.noreply.github.com>
2024-10-18 13:48:06 -0300

    fix: structuring of the project patient

---
+++ src/main/java/br/com/senai/medicalone/dtos/patient/PatientResponseDTO.java
@@ -0,0 +1,4 @@
+package br.com.senai.medicalone.dtos.patient;
+
+public class PatientResponseDTO {
+}
---
+++ src/main/java/br/com/senai/medicalone/services/patient/PatientService.java
@@ -0,0 +1,4 @@
+package br.com.senai.medicalone.services.patient;
+
+public class PatientService {
+}
